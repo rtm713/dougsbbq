c1a562461b67458e60f28df1545f4df71e51df81
rtm713 <rtm713@gmail.com>
2024-08-23 18:46:33 -0500

    changed app routing

--- dougsbbq/src/App.jsx
+++ dougsbbq/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Recipes from './components/Recipes';
 
 function App() {
-
   return (
-    <>
-      <Outlet/>
-    </>
-  )
+    <Router basename="/dougsbbq"> {/* Ensure this matches your GitHub Pages repo name */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="recipes" element={<Recipes />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
