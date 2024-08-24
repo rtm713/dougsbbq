600a1e4a9431781420249bdf4969e15c83f34a90
rtm713 <rtm713@gmail.com>
2024-08-24 00:17:30 -0500

    added landing cards with structure styling

--- dougsbbq/src/components/LandingPage.jsx
+++ dougsbbq/src/components/LandingPage.jsx
@@ -5,6 +5,28 @@ export default function LandingPage() {
     return (
       <>
         <Navbar/>
+        <div id='landingPageCardBox'>
+          <div id='landingFeaturedBox'>
+            <div id='featuredCard'>
+                <h3>Charcoal Grilled Ribs</h3>
+            </div>
+            <div id='featuredMoreCardbox'>
+              <div id='featuredMoreOne' className='featuredMoreCard'>
+                <p>More Recipes</p>
+              </div>
+              <div id='featuredMoreTwo'className='featuredMoreCard'>
+                <p>Sauces</p>
+              </div>
+              <div id='featuredMoreThree'className='featuredMoreCard'>
+                <p>Seasonings</p>
+              </div>
+            </div>
+          </div>
+          <div id='landingBioCard'>
+            <img src="" alt="" />
+            <p>this is where the description will go h uidshu idhuih duisfh uih fuidh fuidsbhfb dsu fuu hu iufuig sfghgh hfbhiuh ui giusb gjkb fhgbig hfbgjhj gnuf kjgnfjngj sfhg jkfb gjkb fjngjkf jk fgbjf gjk fjk gf gjfgjnfjk sgui jhgjk j huhu</p>
+          </div>
+        </div>
       </>
     );
   }
