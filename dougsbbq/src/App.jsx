1dfee6e0b99379bb64c29e72190558259cd68545
rtm713 <rtm713@gmail.com>
2024-08-23 19:04:43 -0500

    test

--- dougsbbq/src/App.jsx
+++ dougsbbq/src/App.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Recipes from './components/Recipes';
+import './App.css'
 import { Outlet } from "react-router-dom";
 
 function App() {
+
   return (
-    <Router basename="/dougsbbq"> {/* Ensure this matches your GitHub Pages repo name */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="recipes" element={<Recipes />} />
-        {/* Add other routes as needed */}
-      </Routes>
-      <Outlet />
-    </Router>
-  );
+    <>
+      <h1>DOUGS BBQ TEST h1</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
