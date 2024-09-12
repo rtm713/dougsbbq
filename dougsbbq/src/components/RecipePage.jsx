121ce43eab8f1d1ea9345e111bcd5184f841f0eb
rtm713 <rtm713@gmail.com>
2024-09-12 18:07:40 -0500

    started styling ind recipe page

--- dougsbbq/src/components/RecipePage.jsx
+++ dougsbbq/src/components/RecipePage.jsx
@@ -6,12 +6,25 @@ export default function RecipePage() {
       <>
         <Navbar/>
         <div className="recipesMainCard">
-        <div className='indRecipeMainBox'>
             <h2>Charcoal Grilled Ribs</h2>
-            <iframe width="420" height="315"
-              src="https://www.youtube.com/embed/tG2ro7UZOUE">
-          </iframe>
-          <div className='indRecipeSideCard'>
+        <div className='indRecipeMainBox'>
+            <div className='indRecipeSideCardOne'>
+                <iframe width="420" height="315"
+                  src="https://www.youtube.com/embed/tG2ro7UZOUE">
+                </iframe>
+                <div className='instructionDiv'>
+            <h4>Instructions</h4>
+            <ol>
+              <li>first do this hfuf uidh uifh duifh duih afuidhfui h uifah uih ufdhjahudh fkjhu huiah ufh hfjusua hfuds</li>
+              <li>Then do this hufdhui h fhuydg hd hgyhb yu gahyg yufg ayufg jhydgfyu gdjfyhgyjh afyhjg fyg by gajhgyu syag yug jyhgyh gyhg djhsa ghy fhjdgyg yg dyg yg dysgf ysd</li>
+              <li> then maybe try this hfujidh uifhiusha fu hdaf</li>
+              <li>and also do this huih uifh sudihf uidh ui hsuafih uifd huih fuihsduf hfuid hsuih uhh uiy fsyu yg gayu gy ggyg yfyg  ya</li>
+              <li>but definitely do this jiu huifhs udhfui dhauih udhauhs auih usahu dushfug ud gfas gfyg d</li>
+              <li>then finally do this ih fuidhuih uds hfui huh dsuahuiah uidf hshd gfgyudsgaf ydgggdyudhygygdyua gghjghjaghj gdshag hjg shaghjdgshajghgsdhaj ghsghj ghj dsghaj gdhjf ghjdsgf </li>
+            </ol>
+          </div>
+            </div>
+          <div className='indRecipeSideCardTwo'>
             <img className='indRecipePic' src="./images/charcoalGrilledRibs.webp" alt="" />
             <div className='recipeTimeDiv'>
               <p>Prep: 10 min</p>
@@ -30,25 +43,14 @@ export default function RecipePage() {
                 </ul>
             </div>
           </div>
-          <div className='instructionDiv'>
-            <h4>Instructions</h4>
-            <ol>
-              <li>first do this hfuf uidh uifh duifh duih afuidhfui h uifah uih ufdhjahudh fkjhu huiah ufh hfjusua hfuds</li>
-              <li>Then do this hufdhui h fhuydg hd hgyhb yu gahyg yufg ayufg jhydgfyu gdjfyhgyjh afyhjg fyg by gajhgyu syag yug jyhgyh gyhg djhsa ghy fhjdgyg yg dyg yg dysgf ysd</li>
-              <li> then maybe try this hfujidh uifhiusha fu hdaf</li>
-              <li>and also do this huih uifh sudihf uidh ui hsuafih uifd huih fuihsduf hfuid hsuih uhh uiy fsyu yg gayu gy ggyg yfyg  ya</li>
-              <li>but definitely do this jiu huifhs udhfui dhauih udhauhs auih usahu dushfug ud gfas gfyg d</li>
-              <li>then finally do this ih fuidhuih uds hfui huh dsuahuiah uidf hshd gfgyudsgaf ydgggdyudhygygdyua gghjghjaghj gdshag hjg shaghjdgshajghgsdhaj ghsghj ghj dsghaj gdhjf ghjdsgf </li>
-            </ol>
-          </div>
-          <div>
+        </div>
+          <div className='extraStuff'>
             <h4>Extra Stuff</h4>
             <p>This is where backstory, any extra tips or whatever will go juidh suihd fhsui fuif hgdhgf yudg hja dgshj gdhjsaghjg dhjghdsgayugew hjufg fbjkh guyhd hfdsg yu fbshjdgfy gdbagdgsjhf gdjhs ajhg dgf gysdgfgds agfyd gfg djhsg yuagyu gfjhgdshjafg yud gfuiydsg fdasf
               fghudis ghfaiud ghfhjlg dhjlg hjg yhujgh fdhg iugh uhg ihfi ugrf
                grui hafuih guifh uigh fui fgyudg fhjgd uyf gdyusgfyg daf gdfya f
             </p>
           </div>
-        </div>
         </div>
       </>
     );
